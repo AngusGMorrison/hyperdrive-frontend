27080d5e565b7e1d8a744db96ee4b85889b557fa
github username <github email address>
2019-11-19 17:09:17 +0000

    Created skeleton for sign-in form

--- src/components/forms/SignInForm.js
+++ src/components/forms/SignInForm.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const SignInForm = () => {
-  return(<div></div>);
+  return(
+    <form className="sign-in">
+      <TextField theme={THEMES.BLUE} icon={} placeholder="Email" />
+      <TextField theme={THEMES.BLUE} icon={} placeholder="Password" />
+    </form>
+  );
 }
 
 export default SignInForm;

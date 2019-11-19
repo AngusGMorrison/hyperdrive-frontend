769bdc123dd9e8060d826689350c7ab22fb7ed4d
github username <github email address>
2019-11-19 17:44:49 +0000

    Completed styling for non-error text fields

--- src/components/forms/fields/TextField.js
+++ src/components/forms/fields/TextField.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './fields.css';
 
 const TextField = ({...props}) => {
 
   return(
-    <div class="icon-text-field">
+    <div className={`icon-text-field ${props.theme}`}>
       
       <img
         className="field-icon"
@@ -17,7 +18,7 @@ const TextField = ({...props}) => {
         value={props.value}
         onChange={(e) => props.handleChange(e.target.value)}
       />
-      
+
     </div>
   )
 }

423489da9055ad3178d1213df8bf8f33f369354d
github username <github email address>
2019-11-22 13:12:27 +0000

    Created skeleton for XHR file upload

--- src/adapters/driveAPI.js
+++ src/adapters/driveAPI.js
@@ -2,6 +2,14 @@ import API, { BASE_URL } from './API';
 
 const DRIVE_URL = BASE_URL + '/drive'
 
-const uploadFile = formData => {
-
+const uploadFile = file => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', DRIVE_URL, true);
+  xhr.setRequestHeader('Authorization', localStorage.token);
+  xhr.responseType = 'json';
+  xhr.onload = () => {
+    this.response.json()
+      .then(console.log);
+  }
+  xhr.send(file);
 }

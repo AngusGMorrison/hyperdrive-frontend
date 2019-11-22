4df593dd667bc4ad6b3a7871a82bb7b0a4626057
github username <github email address>
2019-11-22 16:41:29 +0000

    Upload progress is reset on upload completion

--- src/adapters/driveAPI.js
+++ src/adapters/driveAPI.js
@@ -2,21 +2,19 @@ import API, { BASE_URL } from './API';
 
 const DRIVE_URL = BASE_URL + '/drive'
 
-const uploadFile = file => {
+const uploadFile = (file, progressCallback, completionCallback) => {
   const xhr = new XMLHttpRequest();
   xhr.open('POST', DRIVE_URL, true);
   xhr.setRequestHeader('Authorization', localStorage.token);
   xhr.responseType = 'json';
-  xhr.upload.onprogress = computeUploadProgress;
-  xhr.onload = () => {
-    console.log(xhr.response);
-  }
+  xhr.upload.onprogress = event => computeUploadProgress(event, progressCallback);
+  xhr.onload = () => completionCallback(xhr.response);
   xhr.send(file);
 }
 
-const computeUploadProgress = event => {
+const computeUploadProgress = (event, callback) => {
   if (event.lengthComputable) {
-    console.log(event.loaded + '/' + event.total);
+    callback({loaded: event.loaded, total: event.total})
   }
 }
 
--- src/components/controlPanel/ControlPanel.js
+++ src/components/controlPanel/ControlPanel.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import driveAPI from '../../adapters/driveAPI';
 
 const ControlPanel = props => {
 
+  const initialProgress = {loaded: 0, total: 1};
+  const [uploadProgress, setUploadProgress] = useState(initialProgress);
+
   const handleSubmit = event => {
     event.preventDefault();
     const file = new FormData(event.target);
-    driveAPI.uploadFile(file);
+    driveAPI.uploadFile(file, setUploadProgress, handleResponse);
+  }
+
+  const handleResponse = response => {
+    setUploadProgress(initialProgress);
+    console.log(response);
   }
 
   return(
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="file">Upload</label>
-      <input type="file" name="file" accept=".txt, image/jpg" />
-      <button>Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="file">Upload</label>
+        <input type="file" name="file" accept=".txt, image/jpg" />
+        <button>Submit</button>
+      </form>
+      <progress value={uploadProgress.loaded} max={uploadProgress.total}></progress>
+    </div>
+    
   )
 }
 

fd0dfa01c35f239e7e07628bfa887a4656a9c2f5
github username <github email address>
2019-11-22 16:03:53 +0000

    uploadFile provides upload progress

--- src/adapters/driveAPI.js
+++ src/adapters/driveAPI.js
@@ -7,10 +7,17 @@ const uploadFile = file => {
   xhr.open('POST', DRIVE_URL, true);
   xhr.setRequestHeader('Authorization', localStorage.token);
   xhr.responseType = 'json';
+  xhr.upload.onprogress = computeUploadProgress;
   xhr.onload = () => {
     console.log(xhr.response);
   }
   xhr.send(file);
+}
+
+const computeUploadProgress = event => {
+  if (event.lengthComputable) {
+    console.log(event.loaded + '/' + event.total);
+  }
 }
 
 const driveAPI = {
